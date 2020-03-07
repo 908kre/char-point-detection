ee46002b94156e3f986467dc3f44d7c3c7498447
h4nyu <yao.ntno@gmail.com>
2020-03-07 02:49:31 +0000

    env

---
+++ app/tracking.py
@@ -0,0 +1,17 @@
+import os
+from mlboard_client.writers import Writer
+MLBOARD_URL = os.getenv('MLBOARD_URL', 'http://192.168.10.8:2020/api/')
+
+if __name__ == "__main__":
+    w = Writer(
+        MLBOARD_URL,
+        'test0',
+        {
+            'p0': 12,
+            'p1': {
+                'p3': 'fasdfa'
+            },
+        },
+    )
+    for i in range(100):
+        w.add_scalars({'aaa' : i, "bbb": i * 2})
