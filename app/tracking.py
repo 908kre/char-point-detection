e0e9767ec4900e3c5283df75d970b99e408c86b1
h4nyu <yao.ntno@gmail.com>
2020-03-18 14:45:59 +0000

    remove file

--- app/tracking.py
+++
@@ -1,9 +0,0 @@
-import os
-from mlboard_client import Writer
-
-MLBOARD_URL = os.getenv("MLBOARD_URL", "http://192.168.10.8:2020")
-
-if __name__ == "__main__":
-    w = Writer(MLBOARD_URL, "test0", {"p0": 12, "p1": {"p3": "fasdfa"},},)
-    for i in range(100):
-        w.add_scalars({"aaa": i, "bbb": i * 2})
