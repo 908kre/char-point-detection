7ac4fac7a68de5502d7101ecd1a2f46ba6f73f40
h4nyu <yao.ntno@gmail.com>
2020-03-20 07:23:50 +0000

    simple 1d unet

--- app/app/models.py
+++ app/app/models.py
@@ -20,3 +20,67 @@ class DoubleConv(nn.Module):
 
     def forward(self, x):  # type: ignore
         return self.double_conv(x)
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels:int, out_channels:int,) -> None:
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool1d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x): # type: ignore
+        return self.maxpool_conv(x)
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+    up: t.Union[nn.Upsample, nn.ConvTranspose1d]
+
+    def __init__(self, in_channels: int, out_channels:int, bilinear:bool=True) -> None:
+        super().__init__()
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='linear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose1d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
+
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2): # type: ignore
+        x1 = self.up(x1)
+        diff = torch.tensor([x2.size()[2] - x1.size()[2]])
+        x1 = F.pad(x1, [diff // 2, diff - diff // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels:int, bilinear:bool=True) -> None:
+        super(UNet, self).__init__()
+        self.in_channels = in_channels
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(in_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 512)
+        self.up1 = Up(1024, 256, bilinear)
+        self.up2 = Up(512, 128, bilinear)
+        self.up3 = Up(256, 64, bilinear)
+        self.up4 = Up(128, 1, bilinear)
+
+    def forward(self, x): # type: ignore
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        return x
--- app/tests/test_model.py
+++ app/tests/test_model.py
@@ -1,4 +1,4 @@
-from app.models import DoubleConv
+from app.models import DoubleConv, UNet
 import torch
 
 
@@ -7,3 +7,9 @@ def test_double_conv() -> None:
     layer = DoubleConv(in_channels=1, out_channels=12,)
     y = layer(x)
     assert y.size() == (1, 12, 100)
+
+def test_unet() -> None:
+    x = torch.randn(1, 1, 256)
+    layer = UNet(in_channels=1)
+    y = layer(x)
+    assert y.size() == x.size()
