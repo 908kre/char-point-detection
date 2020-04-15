e03c7040b269a956ffe659f97135a6b0071071f3
h4nyu <yao.ntno@gmail.com>
2020-04-15 15:15:45 +0000

    fix param

--- app/app/train.py
+++ app/app/train.py
@@ -31,8 +31,8 @@ class Trainer:
     ) -> None:
         self.device = DEVICE
         alpha = 1
-        beta = 1.3
-        gamma = 1.1
+        beta = 1.1
+        gamma = 1.3
         flops_multiplier = alpha * (beta**2) * (gamma ** 2)
         depth = 3 * alpha
         resolution = int(128 * beta)
