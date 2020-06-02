54d85d29070c4ab1e8c682b7a6a8aa3d2b8912b1
xinyuan.yao <yao.ntno@gmail.com>
2020-06-02 12:30:59 +0000

    add ClassificationModel test

--- app/models/efficent_det.py
+++ app/models/efficent_det.py
@@ -119,3 +119,34 @@ class ClassificationModel(nn.Module):
         batch_size, width, height, channels = out1.shape
         out2 = out1.view(batch_size, width, height, self.num_anchors, self.num_classes)
         return out2.contiguous().view(x.shape[0], -1, self.num_classes)
+
+
+class RegressionModel(nn.Module):
+    def __init__(
+        self, num_features_in: int, num_anchors: int = 9, feature_size: int = 256
+    ) -> None:
+        super(RegressionModel, self).__init__()
+
+        self.conv1 = nn.Conv2d(num_features_in, feature_size, kernel_size=3, padding=1)
+        self.act1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act2 = nn.ReLU()
+        self.conv3 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act3 = nn.ReLU()
+        self.conv4 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act4 = nn.ReLU()
+        self.output = nn.Conv2d(feature_size, num_anchors * 4, kernel_size=3, padding=1)
+
+    def forward(self, x):  # type: ignore
+        out = self.conv1(x)
+        out = self.act1(out)
+        out = self.conv2(out)
+        out = self.act2(out)
+        out = self.conv3(out)
+        out = self.act3(out)
+        out = self.conv4(out)
+        out = self.act4(out)
+        out = self.output(out)
+        # out is B x C x W x H, with C = 4*num_anchors
+        out = out.permute(0, 2, 3, 1)
+        return out.contiguous().view(out.shape[0], -1, 4)
--- tests/models/test_effient_det.py
+++ tests/models/test_effient_det.py
@@ -1,5 +1,10 @@
 import torch
-from app.models.efficent_det import ClipBoxes, BBoxTransform
+from app.models.efficent_det import (
+    ClipBoxes,
+    BBoxTransform,
+    RegressionModel,
+    ClassificationModel,
+)
 
 
 def test_clip_boxes() -> None:
@@ -19,3 +24,18 @@ def test_bbox_transform() -> None:
     deltas = torch.Tensor([[[0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1],]])
     fn = BBoxTransform()
     res = fn(boxes, deltas)
+
+
+def test_regression_model() -> None:
+    images = torch.ones((1, 1, 10, 10))
+    fn = RegressionModel(1, num_anchors=9)
+    res = fn(images)
+    assert res.shape == (1, 900, 4)
+
+
+def test_classification_model() -> None:
+    images = torch.ones((1, 100, 10, 10))
+    fn = ClassificationModel(num_features_in=100, num_classes=2)
+    res = fn(images)
+    assert res.shape == (1, 900, 2)
+    print(res.shape)
