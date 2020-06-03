a8dbf1b444b253a0e3399f72c1f260c5adc8d8eb
yao <yao.ntno@gmail.com>
2020-06-03 16:22:26 +0000

    tmp

--- app/models/bifpn.py
+++ app/models/bifpn.py
@@ -1,5 +1,7 @@
+import torch
 import typing as t
 from torch import nn, Tensor
+import torch.nn.functional as F
 from .bottlenecks import MobileV3
 
 
@@ -47,6 +49,24 @@ class Up2d(nn.Module):
         return x
 
 
+class Merge2d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,) -> None:
+        super().__init__()
+        self.merge = MobileV3(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            mid_channels=out_channels // 2,
+            stride=1,
+        )
+
+    def forward(self, inputs: t.List[Tensor]) -> Tensor:
+        for i in inputs:
+            print(i.shape)
+        x = torch.cat(inputs, dim=1)
+        x = self.merge(x)
+        return x
+
+
 FP = t.Tuple[Tensor, Tensor, Tensor, Tensor, Tensor]  # p3, p4, p5, p6, p7
 
 
@@ -54,8 +74,22 @@ class BiFPN(nn.Module):
     def __init__(self, channels: int) -> None:
         super().__init__()
 
+        self.up3 = Up2d(channels)
+        self.up4 = Up2d(channels)
+        self.up5 = Up2d(channels)
+        self.up6 = Up2d(channels)
+        self.up7 = Up2d(channels)
+        self.mid_6 = Merge2d(in_channels=channels * 2, out_channels=channels)
+
+        self.down3 = Down2d(channels)
+        self.down4 = Down2d(channels)
+        self.down5 = Down2d(channels)
+        self.down6 = Down2d(channels)
+        self.down7 = Down2d(channels)
+
     def forward(self, inputs: FP) -> FP:
         p3_in, p4_in, p5_in, p6_in, p7_in = inputs
-        print(p3_in)
+        p7_up = self.up7(p7_in, p6_in)
+        #  mid_6_out = self.mid_6([p7_down, p6_in])
 
         return p3_in, p4_in, p5_in, p6_in, p7_in
--- tests/models/test_bipfn.py
+++ tests/models/test_bipfn.py
@@ -3,7 +3,9 @@ from app.models.bifpn import BiFPN
 
 
 def test_bifpn() -> None:
-    req = (torch.ones((1, 3, 10, 10)) for _ in range(5))
-    m = BiFPN(channels=1)
+    req = (
+        torch.ones((1, 128, 1024 // (2 ** i), 1024 // (2 ** i))) for i in range(3, 8)
+    )
+    m = BiFPN(channels=128)
     res = m(req)
     #  assert req.shape == res.shape
