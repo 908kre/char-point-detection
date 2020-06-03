f4315f07d77f7ca6cb278b07cbe89dce956f13e8
coz.a <coz.a.github@gmail.com>
2020-06-04 08:28:46 +0900

    add KuzushijiDataset 宗派の違いを感じるので後から相談 * global config * entity (image, bbox) * mode (train / test)

--- app/dataset.py
+++ app/dataset.py
@@ -1,9 +1,11 @@
-import torch
-import numpy as np
+from pathlib import Path
 import typing as t
-from app.entities import Images
+import numpy as np
+import pandas as pd
+import torch
 from torch.utils.data import Dataset
 from albumentations.pytorch.transforms import ToTensorV2
+from .entities import Images
 
 Row = t.Tuple[t.Any, t.Any, t.Any]
 Batch = t.Sequence[Row]
@@ -39,3 +41,30 @@ class TrainDataset(Dataset):
         labels = torch.ones((len(box_arrs),))
 
         return image_arr, box_arrs, labels
+
+
+class KuzushijiDataset(Dataset):
+
+    def __init__(self, image_dir: str, annot_file: str):
+        self.image_dir = Path(image_dir)
+        annot = pd.read_csv(annot_file)
+        self.image_ids = annot.image_id.values
+        self.bboxes = [self.decode_label(_) for _ in annot.labels.values]
+
+    def __getitem__(self, idx: int) -> dict:
+        image_id = self.image_ids[idx]
+        image = cv2.imread(str(self.image_dir / f"{image_id}.jpg"))[..., ::-1].copy()
+        bboxes = self.bboxes[idx]
+        return dict(image_id=idx, image=image, bboxes=bboxes)
+
+    def __len__(self) -> int:
+        return len(self.image_ids)
+
+    @staticmethod
+    def decode_label(bboxes_str: str) -> np.ndarray:
+        tokens = bboxes_str.split(" ")
+        bboxes = []
+        for idx in range(0, len(tokens), 5):
+            bbox = [int(_) for _ in tokens[idx + 1:idx + 5]]
+            bboxes.append(bbox)
+        return np.array(bboxes)
