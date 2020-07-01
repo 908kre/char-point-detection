8a69c5860faeeff96e0881bcf2067e5df5dd43da
xinyuan-yao <yao.ntno@gmail.com>
2020-07-01 22:01:09 +0000

    add eval

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 from torchvision.ops.boxes import box_iou
-from sklearn.metrics import average_precision_score
+from sklearn.metrics import average_precision_score, precision_recall_curve, auc
 
 
 def coco_to_pascal(boxes):
@@ -11,31 +11,44 @@ def coco_to_pascal(boxes):
 
 
 def sweep_average_precision(
-    true_boxes,
-    pred_boxes,
-    confidences,
-    iou_thresholds=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75],
-):
+        true_boxes, pred_boxes, confidences,
+        iou_thresholds=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75]):
     """
+    compute ap, precision @ f1-best, recall @ f1-best.
     boxes are expected to be in pascal voc (x1, y1, x2, y2) format.
     """
     pred_boxes = pred_boxes[torch.argsort(confidences, descending=True)]
     if len(true_boxes) == 0:
-        return 1.0 if len(pred_boxes) == 0 else 0.0
-    if len(pred_boxes) == 0:
-        return 0.0
+        if len(pred_boxes) == 0:
+            ap = 1.0
+            precision = 1.0
+            recall = 1.0
+            threshold = 0.5
+        else:
+            ap = 0.0
+            precision = 0.0
+            recall = 0.0
+            threshold = 0.5
+        return ap, precision, recall, threshold
+    else:
+        if len(pred_boxes) == 0:
+            return 0.0, 0.0, 0.0, 0.5
+
     iou_matrix = box_iou(pred_boxes, true_boxes)
-    return np.mean(
-        [
-            compute_average_precision(iou_matrix, iou_threshold, confidences)
-            for iou_threshold in iou_thresholds
-        ]
-    )
+    
+    ap, precision, recall, threshold = [], [], [], []
+    for iou_threshold in iou_thresholds:
+        ap_, precision_, recall_, threshold_ = compute_average_precision(iou_matrix, iou_threshold, confidences)
+        ap.append(ap_)
+        precision.append(precision_)
+        recall.append(recall_)
+        threshold.append(threshold_)
+
+    return np.mean(ap), np.mean(precision), np.mean(recall), np.mean(threshold)
 
 
 def compute_average_precision(
-    iou_matrix: torch.Tensor, iou_threshold: float, confidences: torch.Tensor
-) -> float:
+        iou_matrix: torch.Tensor, iou_threshold: float, confidences: torch.Tensor) -> float:
     iou_matrix = iou_matrix.clone()
     n_pred = iou_matrix.size(0)
     n_true = iou_matrix.size(1)
@@ -52,4 +65,12 @@ def compute_average_precision(
         else:
             gt[pred_idx] = 0
     fn = n_true - match
-    return average_precision_score(gt[: n_pred + fn], scores[: n_pred + fn])
+    y_true = gt[:n_pred + fn]
+    probas_pred = scores[:n_pred + fn]
+    precision, recall, thresholds = precision_recall_curve(y_true, probas_pred)
+    ap = auc(recall, precision)
+    f1_score = 2 * precision * recall / (precision + recall + 1e-6)
+    best_idx = np.argmax(f1_score)
+    rep_precision, rep_recall = precision[best_idx], recall[best_idx]
+    rep_threshold = thresholds[best_idx]
+    return ap, rep_precision, rep_recall, rep_threshold
